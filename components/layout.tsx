42ec36977a03460493d81f83b4fce5cbc510ff3e
scalalang2 <cailhuiris@gmail.com>
2022-09-20 23:04:06 +0900

    feat: added lahout

---
+++ components/layout.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+type Props = {
+    children: React.ReactNode
+}
+
+const Layout = ({ children }: Props) => {
+    return (
+        <>
+            <div>
+                { children }
+            </div>
+        </>
+    )
+}
+
+export default Layout
