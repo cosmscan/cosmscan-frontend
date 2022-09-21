132e6d5d89a3492b31c18232a7660b41cabb2020
scalalang2 <cailhuiris@gmail.com>
2022-09-22 00:47:50 +0900

    feat: transaction detail styling

--- pages/transactions/[chain]/[hash].tsx
+++ pages/transactions/[chain]/[hash].tsx
@@ -10,35 +10,121 @@ const ViewTransactionPage: NextPage = () => {
                 </div>
                 <div className="overflow-hidden bg-gray-800 shadow sm:rounded-lg mt-5">
                     <div className="px-4 py-5 sm:px-6">
-                        <h3 className="text-lg font-medium leading-6">Overview</h3>
+                        <h3 className="text-lg font-medium leading-6 text-green-300">Overview</h3>
                     </div>
                     <div className="px-4 py-5 sm:p-0">
                         <dl>
                             <div className="py-4 sm:grid sm:grid-cols-4 sm:gap-4 sm:py-5 sm:px-6">
-                                <dt className="font-medium text-gray-300">Height</dt>
-                                <dd className="mt-1 sm:col-span-3 sm:mt-0">500</dd>
+                                <dt className="font-medium text-gray-500">TxHash</dt>
+                                <dd className="mt-1 sm:col-span-3 sm:mt-0">1C919EED21DE4C690592962D6AFC66D63985317F3D254D95EAEEBE94D2F29D46</dd>
                             </div>
                             <div className="py-4 sm:grid sm:grid-cols-4 sm:gap-4 sm:py-5 sm:px-6">
-                                <dt className="font-medium text-gray-300">Block Time</dt>
+                                <dt className="font-medium text-gray-500">Status</dt>
+                                <dd className="mt-1 sm:col-span-3 sm:mt-0">Success</dd>
+                            </div>
+                            <div className="py-4 sm:grid sm:grid-cols-4 sm:gap-4 sm:py-5 sm:px-6">
+                                <dt className="font-medium text-gray-500">Height</dt>
+                                <dd className="mt-1 sm:col-span-3 sm:mt-0">100</dd>
+                            </div>
+                            <div className="py-4 sm:grid sm:grid-cols-4 sm:gap-4 sm:py-5 sm:px-6">
+                                <dt className="font-medium text-gray-500">Time</dt>
                                 <dd className="mt-1 sm:col-span-3 sm:mt-0">April, 4 2022 10:24:22</dd>
                             </div>
                             <div className="py-4 sm:grid sm:grid-cols-4 sm:gap-4 sm:py-5 sm:px-6">
-                                <dt className="font-medium text-gray-300">Block Hash</dt>
-                                <dd className="mt-1 sm:col-span-3 sm:mt-0">1C919EED21DE4C690592962D6AFC66D63985317F3D254D95EAEEBE94D2F29D46</dd>
+                                <dt className="font-medium text-gray-500">Fee</dt>
+                                <dd className="mt-1 sm:col-span-3 sm:mt-0">1.000000 ATOM</dd>
                             </div>
                             <div className="py-4 sm:grid sm:grid-cols-4 sm:gap-4 sm:py-5 sm:px-6">
-                                <dt className="font-medium text-gray-300">Previous Block Hash</dt>
-                                <dd className="mt-1 sm:col-span-3 sm:mt-0">1C919EED21DE4C690592962D6AFC66D63985317F3D254D95EAEEBE94D2F29D46</dd>
+                                <dt className="font-medium text-gray-500">Gas</dt>
+                                <dd className="mt-1 sm:col-span-3 sm:mt-0">67000 used / 100000 wanted</dd>
                             </div>
                             <div className="py-4 sm:grid sm:grid-cols-4 sm:gap-4 sm:py-5 sm:px-6">
-                                <dt className="font-medium text-gray-300">Proposer</dt>
-                                <dd className="mt-1 sm:col-span-3 sm:mt-0">cosmosvaloper1n229vhepft6wnkt5tjpwmxdmcnfz55jv3vp77d</dd>
-                            </div>
-                            <div className="py-4 sm:grid sm:grid-cols-4 sm:gap-4 sm:py-5 sm:px-6">
-                                <dt className="font-medium text-gray-300">Transactions</dt>
-                                <dd className="mt-1 sm:col-span-3 sm:mt-0">Total 1250</dd>
+                                <dt className="font-medium text-gray-500">Memo</dt>
+                                <dd className="mt-1 sm:col-span-3 sm:mt-0">-</dd>
                             </div>
                         </dl>
+                    </div>
+                    <div className="px-4 py-5 sm:px-1 mx-5 border-t border-gray-500">
+                        <h3 className="text-lg font-medium leading-6">Messages</h3>
+                    </div>
+                    <div className="px-6 pb-5">
+                        <div className="mt-5">
+                            <h4 className="text-gray-400">#1 MsgSend</h4>
+                            <div className="overflow-hidden bg-gray-900 shadow sm:rounded-lg mt-2 py-4">
+                                <dl>
+                                    <div className="py-2 sm:grid sm:grid-cols-5 sm:gap-4 sm:px-6">
+                                        <dt className="font-medium text-gray-500">From Address</dt>
+                                        <dd className="mt-1 sm:col-span-4 sm:mt-0">cosmos1qyqsyqcyq5rqwzqfpg9scrgjl03hn0y2ydfh5u</dd>
+                                    </div>
+                                    <div className="py-2 sm:grid sm:grid-cols-5 sm:gap-4 sm:px-6">
+                                        <dt className="font-medium text-gray-500">To Address</dt>
+                                        <dd className="mt-1 sm:col-span-4 sm:mt-0">cosmos1qyqsyqcyq5rqwzqfpg9scrgjl03hn0y2ydfh5u</dd>
+                                    </div>
+                                    <div className="py-2 sm:grid sm:grid-cols-5 sm:gap-4 sm:px-6">
+                                        <dt className="font-medium text-gray-500">Amount</dt>
+                                        <dd className="mt-1 sm:col-span-4 sm:mt-0">1.000 ATOM</dd>
+                                    </div>
+                                </dl>
+                            </div>
+                        </div>
+                        <div className="mt-5">
+                            <h4 className="text-gray-400">#1 MsgSend</h4>
+                            <div className="overflow-hidden bg-gray-900 shadow sm:rounded-lg mt-2 py-4">
+                                <dl>
+                                    <div className="py-2 sm:grid sm:grid-cols-5 sm:gap-4 sm:px-6">
+                                        <dt className="font-medium text-gray-500">From Address</dt>
+                                        <dd className="mt-1 sm:col-span-4 sm:mt-0">cosmos1qyqsyqcyq5rqwzqfpg9scrgjl03hn0y2ydfh5u</dd>
+                                    </div>
+                                    <div className="py-2 sm:grid sm:grid-cols-5 sm:gap-4 sm:px-6">
+                                        <dt className="font-medium text-gray-500">To Address</dt>
+                                        <dd className="mt-1 sm:col-span-4 sm:mt-0">cosmos1qyqsyqcyq5rqwzqfpg9scrgjl03hn0y2ydfh5u</dd>
+                                    </div>
+                                    <div className="py-2 sm:grid sm:grid-cols-5 sm:gap-4 sm:px-6">
+                                        <dt className="font-medium text-gray-500">Amount</dt>
+                                        <dd className="mt-1 sm:col-span-4 sm:mt-0">1.000 ATOM</dd>
+                                    </div>
+                                </dl>
+                            </div>
+                        </div>
+                        <div className="mt-5">
+                            <h4 className="text-gray-400">#1 MsgSend</h4>
+                            <div className="overflow-hidden bg-gray-900 shadow sm:rounded-lg mt-2 py-4">
+                                <dl>
+                                    <div className="py-2 sm:grid sm:grid-cols-5 sm:gap-4 sm:px-6">
+                                        <dt className="font-medium text-gray-500">From Address</dt>
+                                        <dd className="mt-1 sm:col-span-4 sm:mt-0">cosmos1qyqsyqcyq5rqwzqfpg9scrgjl03hn0y2ydfh5u</dd>
+                                    </div>
+                                    <div className="py-2 sm:grid sm:grid-cols-5 sm:gap-4 sm:px-6">
+                                        <dt className="font-medium text-gray-500">To Address</dt>
+                                        <dd className="mt-1 sm:col-span-4 sm:mt-0">cosmos1qyqsyqcyq5rqwzqfpg9scrgjl03hn0y2ydfh5u</dd>
+                                    </div>
+                                    <div className="py-2 sm:grid sm:grid-cols-5 sm:gap-4 sm:px-6">
+                                        <dt className="font-medium text-gray-500">Amount</dt>
+                                        <dd className="mt-1 sm:col-span-4 sm:mt-0">1.000 ATOM</dd>
+                                    </div>
+                                </dl>
+                            </div>
+                        </div>
+                        <div className="mt-5">
+                            <h4 className="text-gray-400">#1 MsgSend</h4>
+                            <div className="overflow-hidden bg-gray-900 shadow sm:rounded-lg mt-2 py-4">
+                                <dl>
+                                    <div className="py-2 sm:grid sm:grid-cols-5 sm:gap-4 sm:px-6">
+                                        <dt className="font-medium text-gray-500">From Address</dt>
+                                        <dd className="mt-1 sm:col-span-4 sm:mt-0">cosmos1qyqsyqcyq5rqwzqfpg9scrgjl03hn0y2ydfh5u</dd>
+                                    </div>
+                                    <div className="py-2 sm:grid sm:grid-cols-5 sm:gap-4 sm:px-6">
+                                        <dt className="font-medium text-gray-500">To Address</dt>
+                                        <dd className="mt-1 sm:col-span-4 sm:mt-0">cosmos1qyqsyqcyq5rqwzqfpg9scrgjl03hn0y2ydfh5u</dd>
+                                    </div>
+                                    <div className="py-2 sm:grid sm:grid-cols-5 sm:gap-4 sm:px-6">
+                                        <dt className="font-medium text-gray-500">Amount</dt>
+                                        <dd className="mt-1 sm:col-span-4 sm:mt-0">1.000 ATOM</dd>
+                                    </div>
+                                </dl>
+                            </div>
+                        </div>
+                        
                     </div>
                 </div>
             </div>
